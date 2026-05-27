1e91154ca43407145b87c2dc470769dbed4d7241
voluyen <voluyen@gmail.com>
2026-05-27 19:00:19 +0700

    .

--- data_utils/lm_datasets.py
+++ data_utils/lm_datasets.py
@@ -55,8 +55,13 @@ class LMTrainDataset(Dataset):
         source_len = 1
         
         prompt = None
-        if 65535 in input_ids:
-            source_len = np.where(input_ids==65535)[0][0]
+        sentinel = None
+        for _s in (65535, 4294967295):
+            if _s in input_ids:
+                sentinel = _s
+                break
+        if sentinel is not None:
+            source_len = np.where(input_ids==sentinel)[0][0]
             prompt = input_ids[:source_len]
             input_ids = np.concatenate([input_ids[:source_len], input_ids[source_len+1:]], axis=0)
         input_ids = input_ids[:self.max_length]
